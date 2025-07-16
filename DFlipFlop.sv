f9a99af1424a429ab0a2d10aa16c5a6cb886cf07
Antariksh Krishnan <113874137+antarikshrk@users.noreply.github.com>
2025-07-16 07:38:47 -0400

    Update DFlipFlop.sv
    Final Commit

--- DFlipFlop.sv
+++ DFlipFlop.sv
@@ -1,18 +1,3 @@
-//***********************************************************
-// ECE 3058 Architecture Concurrency and Energy in Computation
-//
-// RISCV Processor System Verilog Behavioral Model
-//
-// School of Electrical & Computer Engineering
-// Georgia Institute of Technology
-// Atlanta, GA 30332
-//
-//  Module:     core_tb
-//  Functionality:
-//      System Verilog of a DFlipFlop
-//
-//***********************************************************
-
 module DFlipFlop #(
   parameter DATA_WIDTH = 32
 ) (
